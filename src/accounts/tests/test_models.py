b1e232a2182904da67f9d107d823b45140f17186
flesmes <flesmeszab@gmail.com>
2025-12-14 16:53:40 +0100

    Token model

--- src/accounts/tests/test_models.py
+++ src/accounts/tests/test_models.py
@@ -2,7 +2,7 @@ from django.contrib import auth
 from django.core.exceptions import ValidationError
 from django.test import TestCase
 
-from accounts.models import User
+from accounts.models import Token, User
 
 class UserModelTest(TestCase):
 
@@ -19,3 +19,11 @@ class UserModelTest(TestCase):
   def test_email_is_primary_key(self):
     user = User(email='name@example.com')
     self.assertEqual(user.pk, 'name@example.com')
+
+
+class TokenModelTest(TestCase):
+
+  def test_links_user_with_auto_generated_uid(self):
+    token1 = Token.objects.create(email='name@example.com')
+    token2 = Token.objects.create(email='name@example.com')
+    self.assertNotEqual(token1.uid, token2.uid)
