df2cfa7c621faca24e6ed327284cc9a904b65af3
flesmes <flesmeszab@gmail.com>
2025-12-15 19:05:27 +0100

    Done send_login_email view

--- src/accounts/tests/test_views.py
+++ src/accounts/tests/test_views.py
@@ -3,7 +3,7 @@ from unittest import mock
 from django.test import TestCase
 from django.urls import reverse
 
-import accounts.views
+from accounts.models import Token
 
 class SendLoginEmailViewTest(TestCase):
 
@@ -13,19 +13,6 @@ class SendLoginEmailViewTest(TestCase):
       data={'email': 'name@example.com'}
     )
     self.assertRedirects(response, reverse('home')) 
-
-  @mock.patch('accounts.views.send_mail')
-  def test_sends_mail_to_address_from_post(self, mock_send_mail):
-    self.client.post(
-      reverse('send_login_email'), 
-      data={'email': 'edith@example.com'}
-    )
-
-    self.assertTrue(mock_send_mail.called)
-    (subject, body, from_email, to_list), kwargs = mock_send_mail.call_args
-    self.assertEqual(subject, 'Your login link for Superlists')
-    self.assertEqual(from_email, 'noreply@flesmes.com')
-    self.assertEqual(to_list, ['edith@example.com'])
 
   def test_adds_success_message(self):
     response = self.client.post(
@@ -40,3 +27,41 @@ class SendLoginEmailViewTest(TestCase):
       'Check your email, we\'ve sent you a link you can use to log in.'
     )
     self.assertEqual(message.tags, 'success')
+
+  @mock.patch('accounts.views.send_mail')
+  def test_sends_mail_to_address_from_post(self, mock_send_mail):
+    self.client.post(
+      reverse('send_login_email'), 
+      data={'email': 'edith@example.com'}
+    )
+
+    self.assertTrue(mock_send_mail.called)
+    (subject, body, from_email, to_list), kwargs = mock_send_mail.call_args
+    self.assertEqual(subject, 'Your login link for Superlists')
+    self.assertEqual(from_email, 'noreply@flesmes.com')
+    self.assertEqual(to_list, ['edith@example.com'])
+
+  def test_creates_token_associated_with_email(self):
+    self.client.post(
+      reverse('send_login_email'), 
+      data={'email': 'edith@example.com'}
+    )
+    token = Token.objects.get()
+    self.assertEqual(token.email, 'edith@example.com')
+
+  @mock.patch('accounts.views.send_mail')
+  def test_sends_link_to_login_using_token_uid(self, mock_send_mail):
+    self.client.post(
+      reverse('send_login_email'), 
+      data={'email': 'edith@example.com'}
+    )
+    token = Token.objects.get()
+    expected_url = f'http://testserver/accounts/login?token={token.uid}'
+    (_, body, _, _), _ = mock_send_mail.call_args
+    self.assertIn(expected_url, body)
+
+class LoginViewTest(TestCase):
+
+  def test_redirects_to_home_page(self):
+    response = self.client.get(reverse('login', query={'token': 'abcd123'}))
+    self.assertRedirects(response, reverse('home'))
--- src/accounts/views.py
+++ src/accounts/views.py
@@ -3,14 +3,22 @@ from django.core.mail import send_mail
 from django.shortcuts import redirect
 from django.urls import reverse
 
+from accounts.models import Token
+
 def send_login_email(request):
   SUBJECT = 'Your login link for Superlists'
   FROM_EMAIL = 'noreply@flesmes.com'
   to_email = request.POST['email']
-  body = 'Use this link to log in'
+  token = Token.objects.create(email=to_email)
+  relative_url = reverse('login', query={'token': str(token.uid)})
+  url = request.build_absolute_uri(relative_url)
+  body = f'Use this link to log in:\n\n{url}'
   send_mail(SUBJECT, body, FROM_EMAIL, [to_email])
   messages.success(
     request,
     'Check your email, we\'ve sent you a link you can use to log in.'
   )
+  return redirect(reverse('home'))
+
+def login(request):
   return redirect(reverse('home'))
